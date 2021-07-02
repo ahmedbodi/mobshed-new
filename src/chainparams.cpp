7947011c6df7bc1d8a6424c7146dffd1ee7cc769
ahmedbodi <ahmed_bodi@msn.com>
2021-07-03 00:12:07 +0100

    [Consensus] Add New DNS Seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,6 +121,8 @@ public:
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
         vSeeds.emplace_back("185.163.118.233");
+        vSeeds.emplace_back("dnsseed.mobshedpools.com");
+        vSeeds.emplace_back("host.mobshedpools.com");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,50);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
